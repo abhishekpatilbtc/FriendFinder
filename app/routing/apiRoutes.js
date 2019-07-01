5205dc5df41ccec68bb7897b01eb14f127e3a3d2
Abhishek Patil <AbhishekPatil@gatech.edu>
2019-07-01 08:40:43 -0400

    routes changes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,9 +10,9 @@ app.get("/api/friends", function(req,res){
     return res.json(friendsArray)
 })
 
-app.post("/api/friends", function(req,res){
-    friendsArray.push(req.body)
-    res.json(false)
-})
+// app.post("/api/friends", function(req,res){
+//     friendsArray.push(req.body)
+//     res.json(false)
+// })
 
 };
