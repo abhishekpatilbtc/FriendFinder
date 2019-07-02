2ef1093adaaa1a410a02bef7d26f06e1658c8247
Abhishek Patil <AbhishekPatil@gatech.edu>
2019-07-02 12:29:20 -0400

    complete

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsArray = [
   {
-    name:"Ahmed",
-  photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    name:"Dino",
+  photo:"https://images.freeimages.com/images/large-previews/20c/tyrannosaurus-dinosaur-picture-1638750.jpg",
   scores:[
       5,
       1,
@@ -15,6 +15,54 @@ var friendsArray = [
       1
     ]
 
+  },
+  {
+    "name": "Bug",
+    "photo": "https://images.freeimages.com/images/large-previews/7e9/ladybird-1367182.jpg",
+    "scores": [
+      4,
+      2,
+      3,
+      5,
+      4,
+      2,
+      3,
+      4,
+      3,
+      2
+    ]
+  },
+  {
+    "name": "Puppy",
+    "photo": "https://images.freeimages.com/images/large-previews/20c/my-puppy-maggie-1362787.jpg",
+    "scores": [
+      2,
+      3,
+      2,
+      5,
+      4,
+      3,
+      1,
+      1,
+      4,
+      2
+    ]
+  },
+  {
+    "name": "Sheep",
+    "photo": "https://images.freeimages.com/images/large-previews/9d0/sheep-1639780.jpg",
+    "scores": [
+      4,
+      5,
+      2,
+      5,
+      4,
+      3,
+      3,
+      1,
+      3,
+      5
+    ]
   }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,9 +10,47 @@ app.get("/api/friends", function(req,res){
     return res.json(friendsArray)
 })
 
-// app.post("/api/friends", function(req,res){
-//     friendsArray.push(req.body)
-//     res.json(false)
-// })
+
+app.post("/api/friends",function(req,res){
+    friendsArray.push(req.body);
+    // send match as a response
+    res.json(searchForAllFriends(friendsArray));
+
+});
+
+
+//sum of differences
+function sumOfDiffBetweenArrays(arrayOfUser,arrayExist){
+    var sum=0;
+    for(let i=0;i<arrayOfUser.length;i++){
+        sum += Math.abs(parseInt(arrayOfUser[i])-parseInt(arrayExist[i]));
+    }
+    return sum;
+}
+
+// Find the match
+function searchForAllFriends(arrayOfFriends){
+    var customArray =[];
+    var indexOfLastElement = arrayOfFriends.length - 1;
+    for(let j = 0;j<indexOfLastElement;j++){
+        var object = {
+            name:arrayOfFriends[j].name,
+            photo:arrayOfFriends[j].photo,
+            score: sumOfDiffBetweenArrays(arrayOfFriends[indexOfLastElement].scores,arrayOfFriends[j].scores)
+        };
+        customArray.push(object);
+    }
+
+    // referencing the below protoype logic
+    var matchedFriend = customArray.hasMin('score');
+    return matchedFriend;
+}
+
+// Search through the array of objects and find minimum attributes
+Array.prototype.hasMin = function(attrib) {
+    return this.reduce(function(prev, curr){ 
+        return prev[attrib] < curr[attrib] ? prev : curr; 
+    });
+ }
 
 };
